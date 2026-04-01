39989ac38b1ce16fc4d558ee6e2dde68e9983f5d
mpk <mpk@process-factory.dk>
2026-04-01 15:06:04 +0200

    WIP

--- ubuntu/msp/setup.sh
+++ ubuntu/msp/setup.sh
@@ -7,5 +7,5 @@ MSP_DIR="${BASE_DIR}/private"
 
 pushd ${MSP_DIR}
 
-[[ -d "${MSP_DIR}/pc-setup"]] || git clone git@github.com:mpeki/pc-setup.git
-[[ -d "${MSP_DIR}/profac-test-plugin"]] || git clone git@github.com:mpk-pf/profac-test-plugin.git
+[[ -d "${MSP_DIR}/pc-setup" ]] || git clone git@github.com:mpeki/pc-setup.git
+[[ -d "${MSP_DIR}/profac-test-plugin" ]] || git clone git@github.com:mpk-pf/profac-test-plugin.git
